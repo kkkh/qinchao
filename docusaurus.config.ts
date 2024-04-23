ca20bccfc2c606242b8b98e034cc585391070770
秦超 <qinchao@43322.cn>
2024-04-23 20:26:58 +0800

    Update docusaurus.config.ts
    搜索功能

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -121,7 +121,7 @@ const config: Config = {
         <p style="display: inline-flex; align-items: center;"><img style="height:20px;margin-right: 0.5rem;" src="/img/police.png" alt="police" height="20"/><a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=${
           beian1.match(/\d+/)?.[0]
         }" >${beian1}</a></p>
-        <p>Copyright © 2020 - PRESENT 秦超 Built with Docusaurus.</p>
+        <p>Copyright © 2024 - PRESENT 秦超 Built with Docusaurus.</p>
         `,
     },
     algolia: {
